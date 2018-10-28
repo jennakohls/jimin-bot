21091c39d6dc02718cc94c00b423fea05b55ab02
jennakohls <jennakohls@gmail.com>
2018-10-28 18:08:59 -0400

    privating stuff lol

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         'VHeTzQGBMRuHA0V3yb219AT9F',
-  consumer_secret:      'sHaG5AX83FHvwSS4LwwOB5A1iZItXbat564LQLypZkHo81BDxg',
-  access_token:         '1055157103084613632-1qozIn1nKJw7tG9o2lPWWvYAPjsYZD',
-  access_token_secret:  'kAgaYRcGN7s6qtvzPIiZKNyvHbUdP15iQFjL1rZifH0mp'
-}
