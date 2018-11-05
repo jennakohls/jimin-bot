a1fa8ab770b3c07617193eb8c207baa9f6961006
jennakohls <jennakohls@gmail.com>
2018-11-04 23:40:14 -0500

    cleaning

--- config1.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         'EwoKiOUiUJ7tRXKVW69UgNwhN',
-  consumer_secret:      'n4n22i6QgCmJ5bxzHiZGFgIYgZHmbuEZ60bnwGyOX6g0tJXqeP',
-  access_token:         '1055157103084613632-HQNH2rBfgwsvXysqBMZTEeJD2K9Oek',
-  access_token_secret:  'gNiLXCnhI3LFC8foYOBqAwY7oYNP08XImXN3NU2q59sTa'
-}
